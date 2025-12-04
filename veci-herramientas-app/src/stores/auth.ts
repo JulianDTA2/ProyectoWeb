b8da1b2c2332ee4471b7792592aab4a78a3ead5d
JulianDTA2 <julian.torres@udla.edu.ec>
2025-12-04 03:16:01 -0500

    commit de prueba 3

--- veci-herramientas-app/src/stores/auth.ts
+++ veci-herramientas-app/src/stores/auth.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: 'http://localhost:3000',
 })
 
 // Definimos la interfaz del Usuario con el nuevo campo 'role'
--- veci-herramientas-app/src/views/NotificationsView.vue
+++ veci-herramientas-app/src/views/NotificationsView.vue
@@ -9,7 +9,7 @@ const router = useRouter()
 const notifications = ref<any[]>([])
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: 'http://localhost:3000',
   headers: { Authorization: `Bearer ${authStore.token}` }
 })
 
